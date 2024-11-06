83a3cabb37047a800c683af5108c3f5d6eaf9c93
Haider <farhan.haider418@gmail.com>
2024-11-06 12:46:01 +0530

    sending PONG

--- src/Server.cpp
+++ src/Server.cpp
@@ -115,8 +115,8 @@ int handle_client(int client_fd) {
       std::cerr << "Failed to write message to socket.\n";
       return -1;
     }
-    if (std::string(buffer).find("END") != std::string::npos)
-      break;
+    // if (std::string(buffer).find("END") != std::string::npos)
+    //   break;
   }
   close(client_fd);
   return 0;
