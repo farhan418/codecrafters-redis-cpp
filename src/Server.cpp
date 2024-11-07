50ddd9c7c296f3bd08e267ff7e8d31560014edf7
Haider <farhan.haider418@gmail.com>
2024-11-07 14:12:19 +0530

    sending PONG

--- src/Server.cpp
+++ src/Server.cpp
@@ -11,114 +11,6 @@
 #include <regex>
 
 int handle_client(int, const struct sockaddr_in&);
-// std::string prepare_reply(const RespParser&);
-class RespParser;
-class RedisCommandCenter;
-
-int main(int argc, char **argv) {
-  // Flush after every std::cout / std::cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
-  // std::cout << "Logs from your program will appear here!\n";
-  // Uncomment this block to pass the first stage
-  
-  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_fd < 0) {
-   std::cerr << "Failed to create server socket\n";
-   return 1;
-  }
-
-  // Since the tester restarts your program quite often, setting SO_REUSEADDR
-  // ensures that we don't run into 'Address already in use' errors
-  int reuse = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-    std::cerr << "setsockopt failed\n";
-    return 1;
-  }
-  
-  struct sockaddr_in server_addr;
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = INADDR_ANY;
-  server_addr.sin_port = htons(6379);
-  
-  if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
-    std::cerr << "Failed to bind to port 6379\n";
-    return 1;
-  }
-  
-  int connection_backlog = 5;
-  if (listen(server_fd, connection_backlog) != 0) {
-    std::cerr << "listen failed\n";
-    return 1;
-  }
-  
-  struct sockaddr_in client_addr;
-  int client_addr_len = sizeof(client_addr);
-  
-  while(true) {
-    memset(&client_addr, 0, sizeof(client_addr));
-    std::cout << "Waiting for a client to connect...\n";
-
-    int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-    if (client_fd == -1) {
-      std::cerr << "Failed to accept client connection\n";
-      return 1;
-    }
-    std::cout << "Client connected\n";
-     
-    std::thread t([client_fd, client_addr](){
-      try {
-        handle_client(client_fd, client_addr);
-      }
-      catch(const std::exception& e) {
-        std::cerr << "\nException occurred in thread: " << e.what() << std::endl;
-      }
-      catch(...) {
-        std::cerr << "\nUnknown exception occurred in thread.\n";
-      }
-    });
-    t.detach();
-  }
-  close(server_fd);
-  return 0;
-}
-
-int handle_client(int client_fd, const struct sockaddr_in& client_addr) {
-  int n = 0;
-  char buffer[1024];
-  RespParser resp_parser;
-  RedisCommandCenter rcc;
-
-  while(1) {
-    memset(buffer, 0, sizeof(buffer));  // bzero is also deprecated POSIX function
-    n = read(client_fd, buffer, sizeof(buffer));
-    if (n < 0) {
-      std::cerr << "Error reading from socket.\n";
-      return -1;
-    }
-    resp_parser.resetParser(buffer);
-    while(!resp_parser.isParsedAllTokens()) {
-      std::vector<std::string> command = resp_parser.deserialize(resp_parser.parseNextToken(""));
-      std::string response_str = rcc.process(command);
-
-      memset(buffer, 0, sizeof(buffer));
-      memcpy(buffer, response_str.c_str(), response_str.length());
-      n = write(client_fd, buffer, response_str.length());
-      std::cerr << "\nSent " << n <<" bytes : " << buffer << std::endl;
-      if (n < 0) {
-        std::cerr << "Failed to write message to socket.\n";
-        return -1;
-      }
-      if (std::string(buffer).find("END") != std::string::npos)
-        break;
-    } 
-  }
-  close(client_fd);
-  return 0;
-}
-
 class RespParser {
 private:
   bool isParsed;
@@ -306,3 +198,108 @@ public:
     return response;
   }
 };
+
+
+int main(int argc, char **argv) {
+  // Flush after every std::cout / std::cerr
+  std::cout << std::unitbuf;
+  std::cerr << std::unitbuf;
+
+  // You can use print statements as follows for debugging, they'll be visible when running tests.
+  // std::cout << "Logs from your program will appear here!\n";
+  // Uncomment this block to pass the first stage
+  
+  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_fd < 0) {
+   std::cerr << "Failed to create server socket\n";
+   return 1;
+  }
+
+  // Since the tester restarts your program quite often, setting SO_REUSEADDR
+  // ensures that we don't run into 'Address already in use' errors
+  int reuse = 1;
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+    std::cerr << "setsockopt failed\n";
+    return 1;
+  }
+  
+  struct sockaddr_in server_addr;
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_port = htons(6379);
+  
+  if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
+    std::cerr << "Failed to bind to port 6379\n";
+    return 1;
+  }
+  
+  int connection_backlog = 5;
+  if (listen(server_fd, connection_backlog) != 0) {
+    std::cerr << "listen failed\n";
+    return 1;
+  }
+  
+  struct sockaddr_in client_addr;
+  int client_addr_len = sizeof(client_addr);
+  
+  while(true) {
+    memset(&client_addr, 0, sizeof(client_addr));
+    std::cout << "Waiting for a client to connect...\n";
+
+    int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+    if (client_fd == -1) {
+      std::cerr << "Failed to accept client connection\n";
+      return 1;
+    }
+    std::cout << "Client connected\n";
+     
+    std::thread t([client_fd, client_addr](){
+      try {
+        handle_client(client_fd, client_addr);
+      }
+      catch(const std::exception& e) {
+        std::cerr << "\nException occurred in thread: " << e.what() << std::endl;
+      }
+      catch(...) {
+        std::cerr << "\nUnknown exception occurred in thread.\n";
+      }
+    });
+    t.detach();
+  }
+  close(server_fd);
+  return 0;
+}
+
+int handle_client(int client_fd, const struct sockaddr_in& client_addr) {
+  int n = 0;
+  char buffer[1024];
+  RespParser resp_parser;
+  RedisCommandCenter rcc;
+
+  while(1) {
+    memset(buffer, 0, sizeof(buffer));  // bzero is also deprecated POSIX function
+    n = read(client_fd, buffer, sizeof(buffer));
+    if (n < 0) {
+      std::cerr << "Error reading from socket.\n";
+      return -1;
+    }
+    resp_parser.resetParser(buffer);
+    while(!resp_parser.isParsedAllTokens()) {
+      std::vector<std::string> command = resp_parser.deserialize(resp_parser.parseNextToken(""));
+      std::string response_str = rcc.process(command);
+
+      memset(buffer, 0, sizeof(buffer));
+      memcpy(buffer, response_str.c_str(), response_str.length());
+      n = write(client_fd, buffer, response_str.length());
+      std::cerr << "\nSent " << n <<" bytes : " << buffer << std::endl;
+      if (n < 0) {
+        std::cerr << "Failed to write message to socket.\n";
+        return -1;
+      }
+      if (std::string(buffer).find("END") != std::string::npos)
+        break;
+    } 
+  }
+  close(client_fd);
+  return 0;
+}
