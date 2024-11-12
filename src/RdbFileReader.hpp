ad77651d97814fa4783e712b904d8ecb8f435486
Haider <farhan.haider418@gmail.com>
2024-11-12 17:49:13 +0530

    JZ6

---
+++ src/RdbFileReader.hpp
@@ -0,0 +1,314 @@
+#ifndef RDBFILEREADER_HPP
+#define RDBFILEREADER_HPP
+
+#include <iostream>
+#include <ifstream>
+#include <string>
+#include <cstdint>
+#include "RedisDataStore.hpp"
+#include "RedisCommandCenter.hpp"
+
+enum class ValueType {
+    StringEncoding = 0,
+    ListEncoding = 1,
+    SetEncoding = 2
+};
+
+class RdbFileReader {
+public:
+    RdbFileReader() {
+        reset();
+    }
+
+    int readFile(const std::string& filename) {
+        reset();
+        this->filename = filename;
+        rdb_file.open(this->filename, std::ios::binary);
+        if (!rdb_file.is_open()) {
+            std::cerr << "\nsomething went wrong, cannot open file : " << filename << std::endl;
+            return 1;
+        }
+        if (0 != read_header_and_metadata())
+            return 1;
+
+        if (0 != read_database())
+            return 1;
+
+        rdb_file.close();
+        return 0;
+    }
+
+    ~RdbFileReader() {
+        if (rdb_file.is_open()) {
+            rdb_file.close();
+        }
+    }
+private:
+    // struct File {
+    // public :
+    //     File(const std::string& filename, const std::string) {}
+    //     ~File() {}
+    // private:
+    // }
+    int reset() {
+        cursor_index = 0;
+        filename = "";
+        if (rdb_file.is_open()) {
+            rdb_file.close();
+        }
+        return 0;
+    }
+
+    int read_header_and_metadata() {
+        char value[6];
+        memset(value, 0, sizeof(value));
+        for(int i = 0; i < 5; i++)
+            value[i] = read_byte();
+        
+        if (std::string(value) != "REDIS") {
+            std::cerr << "\nThis file does not follow redis protocol or is not a rdb file, filename : " << filename << std::endl;
+            return 1;
+        }
+
+        memset(value, 0, sizeof(value));
+        uint8_t byte;
+        std::string version;
+        while (byte = read_byte() != 0xFA) {
+            version += std::to_string(byte);
+        }
+        std::cerr << "\nRedis version : " << value << version;
+        std::cerr << "\nMetadata (string encoded key-value pairs): ";
+        
+        while(peek_next_byte() != 0xFE)
+        {
+            std::string key = read_length_encoded_string();
+            std::string value = read_length_encoded_string();
+            std::cerr << "\nKey : " << key << "\nValue : " << value << std::endl;
+        }
+
+        return 0;
+    }
+
+    int read_database() {
+        if (0xFE != read_byte()) {
+            std::cerr << "\nDatabase section should be here.\n";
+            return 1;
+        }
+
+        uint32_t database_index = read_size_encoded_number();
+
+        if (0xFB != read_byte()) {
+            std::cerr << "\nHash table size information section should be here.\n";
+            return 1;
+        }
+
+        std::string key;
+        std::string value;
+        uint32_t count_ht_total = 0;
+        uint32_t count_ht_with_expiry = 0
+
+        uint32_t size_hash_table_total = read_size_encoded_number();
+        uint32_t size_hash_table_with_expiry = read_size_encoded_number();
+
+        for(uint32_t i = 0; i < size_hash_table_total; i++) {
+            uint8_t byte = read_byte();
+            uint64_t expiry_time_ms = UINT64_MAX;
+
+            count_ht_total++;
+            if (byte == 0xFC) {
+                expiry_time_ms = read_little_endian_number(num_bytes=8);
+                count_ht_with_expiry++;
+            }
+            else if (byte == 0xFD) {
+                expiry_time_ms = read_little_endian_number(num_bytes=4);
+                count_ht_with_expiry++;
+            }
+            else if (byte == ValueType::StringEncoding) {}
+            else {
+                throw std::runtime_error("\nNot supported valueType for value in key, value pair\n");
+            }
+            
+            read_key_value_pair(key, value);
+            redis_data_store_obj.set_kv(key, value);
+
+            // key = read_length_encoded_string();
+            // value = read_length_encoded_string();
+            
+
+            // if (byte == ValueType::StringEncoding) {  // key-value without expiry
+            //     count_ht_total += 1;
+            // }
+            // else if (byte == 0xFC) {  // with expiry in ms
+            //     uint64_t expiry_time_ms = read_little_endian_number(8);  // 8 bytes
+            //     read_key_value_pair(key, value);
+            //     RedisDataStore::set_kv(key, value, expiry_time_ms);
+            // }
+            // else if (byte == 0xFD) {  // with expiry in seconds
+            //     uint32_t expiry_time_s = read_little_endian_number(4);  // 4 bytes
+            //     read_key_value_pair(key, value);
+            //     RedisDataStore::set_kv(key, value, expiry_time_s);
+            // }
+            // else {
+            //     std::runtime_error("\nInvalid, byte " + std::to_string(byte) + " should have been 00 or FC or FD.\n");
+            // }
+            
+        }
+
+        return 0;
+    }
+
+    int read_key_value_pair(std::string& key, std::string& value) {
+        uint8_t byte = read_byte();
+        switch(byte) {
+            case ValueType::StringEncoding :  // value is String encoded
+            key = read_length_encoded_string();
+            value = read_length_encoded_string();
+            break;
+
+            default :
+            std::cerr << "\nCurrently only supporting ValueType = StringEncoding\n";
+            return 1;
+            break;
+        }
+        return 0;
+    }
+
+    uint8_t peek_next_byte() {
+        std::streampos currentpos = rdb_file.tellg();
+        uint8_t byte = read_byte();
+        cursor_index -= 1;
+        rdb_file.seekg(currentpos);
+        return byte;
+    }
+
+    uint8_t read_byte() {
+        uint8_t byte;
+        rdb_file.read(reinterpret_cast<char*>&byte, 1);
+        if(rdb_file.gcount() != 1) {
+            std::cerr << "\nError reading data from " << filename << std::endl;
+            return 1;
+        }
+        cursor_index += 1;
+        return byte;
+    }
+
+    std::string read_length_encoded_string() {
+        std::string str;
+
+        uint8_t byte = peek_next_byte();
+        uint8_t msb = byte >> 6;
+        if (msb != 3) { // msb is 0 or 1 or 2
+            uint32_t length = read_size_encoded_number();
+            for(uint32_t i = 0; i < length; i++) {
+                str += std::to_string(read_byte());
+            }
+        }
+        else {  // msb = 3
+            byte = byte & 0x3F;  // last 6 bits
+            if (byte == 0) {
+                str = std::to_string(read_little_endian_number(num_bytes=1));
+            }
+            else if (byte == 1) {
+                str = std::to_string(read_little_endian_number(num_bytes = 2));
+            }
+            else if (byte == 2) {
+                str = std::to_string(read_little_endian_number(4));
+            }
+            else if (byte == 3) {  // LZF compression
+                throw std::runtime_error("\nInvalid, compression is not expected in this project.");
+            }
+        }
+
+        // if (msb >= 0 && msb <= 2) {
+        //     length = read_size_encoded_number();
+        //     length = byte & 0b00111111;  // 6 LSBs
+        // }
+        // else if (msb == 1) {
+        //     length = length | (byte & 0b00111111);
+        //     byte = read_byte();
+        //     length = length << 8;
+        //     length = length | byte;  // 14 bit length
+        // }
+        // else if (msb == 2) {
+        //     for (int i = 0; i < 4; i++) {
+        //         byte = read_byte();
+        //         length = length | byte;
+        //         length = length << 8;  // 32 bit length
+        //     }
+        // }
+        // else {  // msb == 3
+            // byte = byte & 0x3F;  // last 6 bits
+            // if (byte == 0) {
+            //     str = read_little_endian(num_bytes=1);
+            // }
+            // else if (byte == 1) {
+            //     str = read_little_endian(num_bytes = 2);
+            // }
+            // else if (byte == 2) {
+            //     str = read_little_endian(4);
+            // }
+            // else if (byte == 3) {  // LZF compression
+            //     throw std::runtime_error("\nInvalid, compression is not expected in this project.");
+            // }
+        // }
+        return str;
+    }
+
+    uint32_t read_size_encoded_number() {
+        uint8_t byte = read_byte();
+        uint8_t msb = byte >> 6;
+        uint32_t length = 0;
+
+        if (msb == 0x00) {
+            length = byte & 0x3F;  // last 6 bits
+        }
+        else if (msb == 0x01) {
+            length = byte & 0x3F;
+            length = length << 8;
+            length = length | read_byte();  // 14 bits
+        }
+        else if (msb == 0x10) {
+            for (int i = 0; i < 4; i++) {
+                length = length << 8;
+                length = length | read_byte();
+            }
+        }
+        else if (msb == 0x11) {
+            std::runtime_error("\nNot a number, but string is stored.");
+            // uint8_t last_6_bits = byte & 0x3F;
+            // if (last_6_bits == 0) {
+            //     length = 1;  // next 1 byte
+            //     length = read_little_endian(1);
+            // }
+            // else if (last_6_bits == 1) {
+            //     length = 2;  // next 2 bytes
+            //     length = read_little_endian(2);
+            // }
+            // else if (last_6_bits == 2) {
+            //     length = 4;  // next 4 bytes
+            //     length = read_little_endian(4);
+            // }
+            // else if (last_6_bits == 3) {
+            //     std::runtime_error("\nInvalid for this project.\n");
+            // }
+        }
+        return length;
+    }
+
+    uint64_t read_little_endian_number_string(int num_bytes) {
+        uint64_t number = 0;  // reads max 8 bytes number
+        for(int i = 0; i < num_bytes; i++) {
+            uint64_t temp = read_byte();
+            temp = temp << (i * 8);
+            number = number | temp;
+        }
+        return number;
+    }
+
+    size_t cursor_index;
+    std::string filename;
+    std::ifstream rdb_file;
+    RedisDataStore redis_data_store_obj;
+};
+
+#endif  // RDBFILEREADER_HPP
---
+++ src/RedisDataStore.hpp
@@ -0,0 +1,155 @@
+#ifndef REDISDATASTORE_HPP
+#define REDISDATASTORE_HPP
+
+#include <iostream>
+#include <string>
+#include <thread>
+#include <mutex>
+#include <chrono>
+#include <optional>
+#include <map>
+#include <queue>
+#include <vector>
+#include <cstdint>
+
+class RedisDataStore {
+public:
+    RedisDataStore() {
+        std::lock_guard<std::mutex> guard(rds_mutex);
+        rds_object_counter++;
+        if (rds_object_counter == 1 && !is_continue_monitoring) {
+            is_continue_monitoring = true;
+            monitor_thread = std::thread(monitor_keys_for_expiry);  // do I need to detach this thread ?
+        }
+    }
+
+    ~RedisDataStore() {
+        std::lock_guard<std::mutex> guard(rds_mutex);
+        rds_object_counter--;
+        if (rds_object_counter == 0) {
+            is_continue_monitoring = false;
+            if (monitor_thread.joinable()) {
+                monitor_thread.join();
+            }
+        }
+    }
+
+    std::optional<std::string> get_kv(const std::string& key) {
+        std::lock_guard<std::mutex> guard(rds_mutex);  // do I need to lock mutex for getting key, value ?
+        auto it = key_value_map.find(key);
+        if (it == key_value_map.end();) {
+            return std::nullopt;
+        }
+        return key_value_map[key];
+    }
+
+    // int set_kv(const std::string& key, const std::string& value) {
+    //     uint8_t return_status = 0;
+    //     try {
+    //         std::lock_guard<std::mutex> guard(rds_mutex);
+    //         key_value_map[key] = value;
+    //     }
+    //     catch(...) {
+    //         return_status = -1;
+    //     }
+    //     return return_status;
+    // }
+
+    int set_kv(const std::string& key, const std::string& value, const uint64_t& expiry_time_ms = UINT64_MAX) {
+        uint8_t return_status = 0;
+        try {
+            std::lock_guard<std::mutex> guard(rds_mutex);
+            key_value_map[key] = value;
+            key_expiry_pq.push({key, expiry_time_ms});
+        }
+        catch (std::exception& e) {
+            return_status = -1;
+        }
+        catch(...) {
+            return_status = -1;
+        }
+        return return_status;
+    }
+
+    int delete_kv(const std::string& key) {
+        std::lock_guard<std::mutex> guard(rds_mutex);
+        auto it = key_value_map.find(key);
+        if (it == key_value_map.end()) {
+            return -1;
+        }
+        key_value_map.erase(key);
+        // if (it = key_expiry_pq.find(key) != key_expiry_pq.end())  // no need to check because erase does not throw exception
+            key_expiry_pq.erase(key);
+        return 0;
+    }
+
+private:
+    void monitor_keys_for_expiry() {
+
+        while(is_continue_monitoring) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            // std::lock_guard<std::mutex> guard(rds_mutex);
+
+            while (!key_expiry_pq.empty()) {
+                auto key_value_pair = key_expiry_pq.top();
+                std::string key = key_value_pair.first;
+                uint64_t key_expiry_time = key_expiry_pq.second;
+                if (key_expiry_time > get_current_time_ms())
+                    break;
+                // delete_kv(key); // but this is slow
+                std::lock_guard<std::mutex> guard(rds_mutex);
+                key_value_map.erase(key);
+                key_expiry_pq.pop();
+            }
+
+            // for(auto it = key_expiry_map.begin(); it != key_expiry_map.end(); ) {
+            //     if (it->second < get_current_time_ms()) {
+            //         // std::lock_guard<std::mutex> guard(key_value_map_mutex);
+            //         std::cerr << "\nDeleting key=" << it->first << ", value=" << key_value_map[it->first] << "\n";
+            //         key_value_map.erase(it->first);
+            //         it = key_expiry_map.erase(it);
+            //     }
+            //     else {
+            //         ++it;
+            //     }
+            // }
+        }
+    }
+
+    uint64_t get_current_time_ms() {
+        auto now = std::chrono::system_clock::now();  // time point object
+        auto duration = now.time_since_epoch();
+        return std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
+            
+    }
+
+    struct ExpiryComparator {
+        bool operator()(const std::pair<std::string, uint64_t>& left, const std::pair<std::string, uint64_t>& right) {
+            return left.second > right.second;  // for min-heap
+        }
+    };
+
+    static std::map<std::string, std::string> key_value_map;
+    static std::priority_queue<std::pair<std::string, uint64_t>, std::vector<std::pair<std::string, uint64_t>>, ExpiryComparator> key_expiry_pq;
+    static bool is_continue_monitoring; 
+    static uint8_t rds_object_counter;
+    static std::thread monitor_thread;
+    static std::mutex rds_mutex;
+    // static std::map<std::string, uint64_t> key_expiry_map;
+    // static std::mutex key_expiry_map_mutex;
+    // static uint64_t daemon_thread_sleep_duration_ms;
+    // static std::vector<std::thread> daemon_thread_pool;
+};
+
+std::map<std::string, std::string> RedisDataStore::key_value_map;
+std::priority_queue<std::pair<std::string, uint64_t>, std::vector<std::pair<std::string, uint64_t>>, ExpiryComparator> RedisDataStore::key_expiry_pq;
+bool RedisDataStore::is_continue_monitoring = false;
+uint8_t RedisDataStore::rds_object_counter;
+std::thread RedisDataStore::monitor_thread;
+std::mutex RedisDataStore::rds_mutex;
+// std::map<std::string, uint64_t> RedisDataStore::key_expiry_map;
+// uint32_t RedisDataStore::daemon_thread_sleep_duration_ms;
+// std::vector<std::thread> RedisDataStore::daemon_thread_pool;
+
+
+#endif  // REDISDATASTORE_HPP
