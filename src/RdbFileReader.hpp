b77922895f9bf6ce3dc3c7e5c022714f445760e6
Haider <farhan.haider418@gmail.com>
2024-11-13 12:19:33 +0530

    JZ6

--- src/RdbFileReader.hpp
+++ src/RdbFileReader.hpp
@@ -11,9 +11,15 @@
 #include "RedisDataStore.hpp"
 #include "RedisCommandCenter.hpp"
 
-#ifndef DEBUG_LOG
-#define DEBUG_LOG(msg) auto now = std::chrono::system_clock::now(); std::time_t now_time = std::chrono::system_clock::to_time_t(now); std::cerr << "[" << now_time << "] [" << __FILE__ << ":" << __LINE__ << "] " << (msg) << std::endl;
-#endif
+// #ifndef DEBUG_LOG
+// #define DEBUG_LOG(msg) auto now = std::chrono::system_clock::now(); std::time_t now_time = std::chrono::system_clock::to_time_t(now); std::cerr << "[" << now_time << "] [" << __FILE__ << ":" << __LINE__ << "] " << (msg) << std::endl;
+// #endif
+
+void DEBUG_LOG(std::string msg) {
+  auto now = std::chrono::system_clock::now();
+  std::time_t now_time = std::chrono::system_clock::to_time_t(now);
+  std::cerr << "[" << now_time << "] [" << __FILE__ << ":" << __LINE__ << "] " << (msg) << std::endl;
+}
 
 enum class ValueType : uint8_t {
     StringEncoding = 0,
--- src/Server.cpp
+++ src/Server.cpp
@@ -22,9 +22,15 @@
 // std::cerr << "[" << now_time << "] [" << __FILE__ << ":" << __LINE__ << "] " << (msg) << std::endl;
 // #endif
 
-#ifndef DEBUG_LOG
-#define DEBUG_LOG(msg) auto now = std::chrono::system_clock::now(); std::time_t now_time = std::chrono::system_clock::to_time_t(now); std::cerr << "[" << now_time << "] [" << __FILE__ << ":" << __LINE__ << "] " << (msg) << std::endl;
-#endif
+// #ifndef DEBUG_LOG
+// #define DEBUG_LOG(msg) auto now = std::chrono::system_clock::now(); std::time_t now_time = std::chrono::system_clock::to_time_t(now); std::cerr << "[" << now_time << "] [" << __FILE__ << ":" << __LINE__ << "] " << (msg) << std::endl;
+// #endif
+
+void DEBUG_LOG(std::string msg) {
+  auto now = std::chrono::system_clock::now();
+  std::time_t now_time = std::chrono::system_clock::to_time_t(now);
+  std::cerr << "[" << now_time << "] [" << __FILE__ << ":" << __LINE__ << "] " << (msg) << std::endl;
+}
 
 int handle_client(int, const struct sockaddr_in&);
 
