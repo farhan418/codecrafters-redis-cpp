779588d9c47d72626fbf881aed0bb3fd56eedca5
Haider <farhan.haider418@gmail.com>
2024-11-13 12:20:32 +0530

    JZ6

--- src/Server.cpp
+++ src/Server.cpp
@@ -26,7 +26,7 @@
 // #define DEBUG_LOG(msg) auto now = std::chrono::system_clock::now(); std::time_t now_time = std::chrono::system_clock::to_time_t(now); std::cerr << "[" << now_time << "] [" << __FILE__ << ":" << __LINE__ << "] " << (msg) << std::endl;
 // #endif
 
-void DEBUG_LOG(std::string msg) {
+static void DEBUG_LOG(std::string msg) {
   auto now = std::chrono::system_clock::now();
   std::time_t now_time = std::chrono::system_clock::to_time_t(now);
   std::cerr << "[" << now_time << "] [" << __FILE__ << ":" << __LINE__ << "] " << (msg) << std::endl;
