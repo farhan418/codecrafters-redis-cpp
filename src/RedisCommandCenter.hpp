6d66ad0239bb1bc27751d248ca0d658cb5586002
Haider <farhan.haider418@gmail.com>
2024-11-14 18:45:23 +0530

    XC1

--- src/RedisCommandCenter.hpp
+++ src/RedisCommandCenter.hpp
@@ -56,6 +56,7 @@ public:
     RedisCommandCenter::set_config_kv("role", "master");
     RedisCommandCenter::set_config_kv("master_replid", "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb");
     RedisCommandCenter::set_config_kv("master_repl_offset", "0");
+    return 0;
   }  
 
   std::string process(const std::vector<std::string>& command) {
