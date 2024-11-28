c5c6b7899f029af09a80f4dd28e8134681c04a20
Haider <farhan.haider418@gmail.com>
2024-11-28 12:44:15 +0530

    CF8

--- src/utility.hpp
+++ src/utility.hpp
@@ -68,7 +68,7 @@ namespace utility {
                     case '\r' : ss << "\\r"; break;
                     case '\n' : ss << "\\n"; break;
                     case '\t' : ss << "\\t"; break;
-                    default : ss << "\\x" << ;std::setw(2) << std::setfill('0') << std::hex << static_cast<int>(c);
+                    default : ss << "\\x" << std::setw(2) << std::setfill('0') << std::hex << static_cast<int>(c);
                 }
             }
         }
